7a8d795a9d7f9aaa74514990239b44bbd075b1a0
Mario de  la Horra <mariodlahorrafalomir@gmail.com>
2025-07-02 08:47:15 +0200

    arreglando conflictos

--- App.java
+++ App.java
@@ -1,6 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+<<<<<<< HEAD
         System.out.println("hola mundo");
     }
+=======
+        System.out.println("Hola mundo");
+        System.out.println("Hola mon");
+        System.out.println("Olá mundo");
+    }   
+>>>>>>> 6abc348 (se añadio el portuges y se solucionaron cambios)
 }
