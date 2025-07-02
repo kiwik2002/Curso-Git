80b8c5de11cf2bc5c9d380ded392599d65f7fcd2
Mario de  la Horra <mariodlahorrafalomir@gmail.com>
2025-07-02 09:01:34 +0200

    añadimos esperanto

--- App.java
+++ App.java
@@ -1,13 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-<<<<<<< HEAD
-        System.out.println("hola mundo");
-    }
-=======
         System.out.println("Hola mundo");
         System.out.println("Hola mon");
         System.out.println("Olá mundo");
+        System.out.println("Saluton mondo");
     }   
->>>>>>> 6abc348 (se añadio el portuges y se solucionaron cambios)
 }
